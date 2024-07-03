990c526106e42f59a86cf768898e258cb0b635ba
alfo0924 <146694784+alfo0924@users.noreply.github.com>
2024-07-03 15:02:13 +0800

    v1.012 commit n push

--- food-cat/src/main/java/fcu/web/Main.java
+++ food-cat/src/main/java/fcu/web/Main.java
@@ -1,11 +1,17 @@
 package fcu.web;
 
+import org.joda.time.DateTime;
+
 public class Main {
     public static void main(String[] args)
     {
         System.out.println("Hello world!");
-
         Student tom=new Student("male","ComputerScienese",3318);
         System.out.println(tom.toString());
+
+        DateTime now = new DateTime();
+        System.out.println(now.toString());
+        System.out.println(now);
+
     }
 }
